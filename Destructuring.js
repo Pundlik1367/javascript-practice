e46e2887489cde5cffe52a75b7ffa73f55a89459
pundlik <pundliksulgekar5222@gmail.com>
2026-07-18 11:37:11 +0530

    Fixed bugs

--- Destructuring.js
+++ Destructuring.js
@@ -1,37 +1,37 @@
 // ---- Destructuring Objects ----
 
-const user = {
+const user1 = {
     name: "John Doe",
     age: 30,    
 }
 
 // without destructuring
-console.log(user.name);
+console.log(user1.name);
 
 
 // with destructuring - Method 1
-const user = { name, age } = {name: "John Doe", age: 30,}
+const { name, age } = {name: "John Doe", age: 30,}
 console.log(name);
 console.log(age);
 
 // with destructuring - Method 2
-const { name: userName, age: userAge } = user;
+const { name: userName, age: userAge } = user1;
 console.log(userName);
 console.log(userAge);
 
 // with destructuring - Method 3
-const { name, age } = user;
-console.log(name);
-console.log(age);
+const { Fullname, Currentage } = user1;
+console.log(Fullname);
+console.log(Currentage);
 
 // ---- Destructuring Arrays ----
 
-const user1 = ["Pundlik", "John", "Jane"];
+const user2 = ["Pundlik", "John", "Jane"];
 
 // without destructuring
-console.log(user1[0]);
-console.log(user1[1]);
-console.log(user1[2]);
+console.log(user2[0]);
+console.log(user2[1]);
+console.log(user2[2]);
 
 // with destructuring method 1
 const [pundlik, john, jane] = ["Pundlik", "John", "Jane"];
@@ -41,14 +41,7 @@ console.log(john);
 console.log(jane);
 
 // with destructuring method 2
-const [pundlik, john, jane] = user1;
-
-console.log(pundlik);
-console.log(john);
-console.log(jane);
-
-// with destructuring method 3
-const [firstName, secondName, thirdName] = user1;
+const [firstName, secondName, thirdName] = user2;
 console.log(firstName);
 console.log(secondName);
 console.log(thirdName);
